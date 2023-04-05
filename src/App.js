56d6929944d01b511975b88b7a39daf2b32b5b61
AndonG7 <gjorgjiev.andon@uklo.edu.mk>
2023-04-05 19:45:19 +0200

    added functionality

--- src/App.js
+++ src/App.js
@@ -1,16 +1,54 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ScoreBoard from "./components/ScoreBoard";
 import PrintCards from "./components/PrintCards";
+import grujo from "./images/grujo.jpg";
+import zaev from "./images/zajko.jpg";
+import obidzuko from "./images/obidzuko.jpg";
+import vasko from "./images/vasko.jpg";
+import caci from "./images/caci.jpg";
+import apasiev from "./images/apasiev.jpg";
+import shuffleArray from "./shuffleArray.js";
 
 function App() {
   const [score, setScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
+  const [clickedCards, setClickedCards] = useState([]);
+  const [shuffledCards, setShuffledCards] = useState([
+    {title: "Грујо", src:grujo, alt:"Nikola Gruevski", id:0},
+    {title: "Заев", src:zaev, alt:"Zoran Zaev", id:1},
+    {title: "Обиџуко", src:obidzuko, alt:"Nikola Gruevski", id:2},
+    {title: "Васко", src:vasko, alt:"Nikola Gruevski", id:3},
+    {title: "Цаци", src:caci, alt:"Nikola Gruevski", id:4},
+    {title: "Апасиев", src:apasiev, alt:"Nikola Gruevski", id:5},
+  ]);
+
+useEffect(() => {
+  setShuffledCards(shuffleArray(shuffledCards));
+}, [clickedCards]);
+
+function handleClick(id) {
+  // Check if the card has already been clicked
+  if (clickedCards.includes(id)) {
+    // Reset the score and clickedCards state
+    setScore(0);
+    setClickedCards([]);
+    alert("Избравте карта што веќе ја имавте избрано!");
+  } else {
+    // Increment the score and add the card to clickedCards state
+    setScore(score + 1);
+    setClickedCards([...clickedCards, id]);
+    // Update the best score if necessary
+    if (score + 1 > bestScore) {
+      setBestScore(score + 1);
+    }
+  }
+}
 
   return (
     <div className="App">
       <h1>Македонска Меморија</h1>
       <ScoreBoard score={score} bestScore={bestScore} />
-      <PrintCards />
+      <PrintCards handleClick={handleClick} cards={shuffledCards} />
     </div>
   );
 }
--- src/components/PrintCards.js
+++ src/components/PrintCards.js
@@ -1,28 +1,11 @@
-import grujo from "../images/grujo.jpg";
-import zaev from "../images/zajko.jpg";
-import obidzuko from "../images/obidzuko.jpg";
-import vasko from "../images/vasko.jpg";
-import caci from "../images/caci.jpg";
-import apasiev from "../images/apasiev.jpg";
-import shuffleArray from "../shuffleArray.js";
 
-function PrintCards() {
-    const cards = [
-        {title: "Грујо", src:grujo, alt:"Nikola Gruevski", key:0},
-        {title: "Заев", src:zaev, alt:"Zoran Zaev", key:1},
-        {title: "Обиџуко", src:obidzuko, alt:"Nikola Gruevski", key:2},
-        {title: "Васко", src:vasko, alt:"Nikola Gruevski", key:3},
-        {title: "Цаци", src:caci, alt:"Nikola Gruevski", key:4},
-        {title: "Апасиев", src:apasiev, alt:"Nikola Gruevski", key:5},
-    ];
-
-    const shuffledCards = shuffleArray(cards);
+function PrintCards(props) {
 
     return (
       <div className="PrintCards">
-        {shuffledCards.map((card, index) => {
+        {props.cards.map((card) => {
                     return (
-                        <div class="card" key={index}>
+                        <div class="card" key={card.id} onClick={(e) => props.handleClick(card.id)} >
                             <img class="card-image" src={card.src} alt={card.alt}></img>
                             <h2 class="card-title">{card.title}</h2>
                         </div>
